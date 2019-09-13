51138fd53814202615d62f31650a3bb0506840e9
teej387 <teej387@yahoo.com>
2019-09-13 18:46:22 -0500

    Set up generateToken() for a jwt in auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res) => {
   // implement registration
@@ -7,5 +9,18 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options)
+}
 
 module.exports = router;
