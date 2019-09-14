01a91b36a601764a4343d43d73f680aeb3c6d2da
teej387 <teej387@yahoo.com>
2019-09-13 19:34:43 -0500

    Set up /login endpoint to validate user and create a token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -34,6 +34,24 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body
+
+  db('users').where({username}).first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+
+        res.status(200).json({
+          message: `${user.username} is logged in.`,
+          token
+        })
+      }else {
+        res.status(401).json({ message: 'Invalid username or password.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: `${err}` })
+    })
 });
 
 function generateToken(user) {
