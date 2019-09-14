17482828af8a2cf52b76b958a7a34cfa493c3874
teej387 <teej387@yahoo.com>
2019-09-13 19:59:11 -0500

    Set up knexfile.js for testing

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,16 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  }
 };
