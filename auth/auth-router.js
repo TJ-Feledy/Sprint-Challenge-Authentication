17e3429c8b1a183e01253d7e2fcce9455c1e5645
teej387 <teej387@yahoo.com>
2019-09-13 19:05:01 -0500

    Set up /register endpoint with bcrypt and a jwt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,8 +2,34 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+const db = require('../database/dbConfig.js')
+
+// endpoints start with /api/auth
+
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash
+
+  db('users').insert(user)
+    .then(ids => {
+      const id = ids[0]
+
+      db('users').where({ id }).first()
+        .then(saved => {
+          const token = generateToken(saved)
+
+          res.status(201).json({
+            user: saved,
+            token
+          })
+        })
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: `${err}` })
+    })
+      
 });
 
 router.post('/login', (req, res) => {
